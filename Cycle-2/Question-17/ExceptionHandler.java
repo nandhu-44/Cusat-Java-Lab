ef946b041a512abfc5d166f04d5c852dabef5725
nandhu-44 <nandhu90040@gmail.com>
2023-07-23 11:24:23 +0530

    Completed Q17

---
+++ Cycle-2/Question-17/ExceptionHandler.java
@@ -0,0 +1,219 @@
+/**
+ * Write a program to create the following GUI
+ * To read elements to an array, divide the sum of elements with the given divisor and print the result. 
+ * Handle all exceptions and alert the user using dialogue.
+ */
+
+import javax.swing.JOptionPane;
+
+public class ExceptionHandler extends javax.swing.JFrame {
+
+    private javax.swing.JButton btn_calculate;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel lbl_divisor;
+    private javax.swing.JLabel lbl_nos;
+    private javax.swing.JLabel lbl_numbers;
+    private javax.swing.JLabel lbl_result;
+    private javax.swing.JTextField txt_divisor;
+    private javax.swing.JTextField txt_nos;
+    private javax.swing.JTextArea txt_numbers;
+    private javax.swing.JTextField txt_result;
+
+    public ExceptionHandler() {
+        initComponents();
+    }
+
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        lbl_nos = new javax.swing.JLabel();
+        txt_nos = new javax.swing.JTextField();
+        lbl_numbers = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        txt_numbers = new javax.swing.JTextArea();
+        lbl_divisor = new javax.swing.JLabel();
+        btn_calculate = new javax.swing.JButton();
+        txt_divisor = new javax.swing.JTextField();
+        lbl_result = new javax.swing.JLabel();
+        txt_result = new javax.swing.JTextField();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Exception Handler");
+
+        lbl_nos.setText("No of Numbers");
+
+        lbl_numbers.setText("Numbers ");
+
+        txt_numbers.setColumns(20);
+        txt_numbers.setRows(5);
+        jScrollPane1.setViewportView(txt_numbers);
+
+        lbl_divisor.setText("Divisor");
+
+        btn_calculate.setText("Calculate");
+        btn_calculate.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_calculateActionPerformed(evt);
+            }
+        });
+
+        txt_divisor.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txt_divisorActionPerformed(evt);
+            }
+        });
+
+        lbl_result.setText("Result");
+
+        txt_result.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txt_resultActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+                jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(15, 15, 15)
+                                .addGroup(jPanel1Layout
+                                        .createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                        .addComponent(lbl_result, javax.swing.GroupLayout.DEFAULT_SIZE,
+                                                javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                        .addComponent(lbl_divisor, javax.swing.GroupLayout.DEFAULT_SIZE,
+                                                javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                        .addComponent(lbl_numbers, javax.swing.GroupLayout.DEFAULT_SIZE,
+                                                javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                        .addComponent(lbl_nos, javax.swing.GroupLayout.DEFAULT_SIZE,
+                                                javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                .addGap(33, 33, 33)
+                                .addGroup(jPanel1Layout
+                                        .createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                                        .addComponent(txt_nos, javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.LEADING,
+                                                javax.swing.GroupLayout.DEFAULT_SIZE, 355, Short.MAX_VALUE)
+                                        .addGroup(jPanel1Layout.createSequentialGroup()
+                                                .addComponent(txt_divisor)
+                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                                .addComponent(btn_calculate))
+                                        .addComponent(txt_result))
+                                .addContainerGap(15, Short.MAX_VALUE)));
+        jPanel1Layout.setVerticalGroup(
+                jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(15, 15, 15)
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addComponent(txt_nos, javax.swing.GroupLayout.PREFERRED_SIZE,
+                                                javax.swing.GroupLayout.DEFAULT_SIZE,
+                                                javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(lbl_nos))
+                                .addGap(34, 34, 34)
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addComponent(lbl_numbers)
+                                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 122,
+                                                javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGap(34, 34, 34)
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addGroup(jPanel1Layout
+                                                .createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                                .addComponent(lbl_divisor)
+                                                .addComponent(txt_divisor, javax.swing.GroupLayout.PREFERRED_SIZE,
+                                                        javax.swing.GroupLayout.DEFAULT_SIZE,
+                                                        javax.swing.GroupLayout.PREFERRED_SIZE))
+                                        .addComponent(btn_calculate))
+                                .addGap(34, 34, 34)
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addComponent(lbl_result)
+                                        .addComponent(txt_result, javax.swing.GroupLayout.PREFERRED_SIZE,
+                                                javax.swing.GroupLayout.DEFAULT_SIZE,
+                                                javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addContainerGap(45, Short.MAX_VALUE)));
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE,
+                                javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE));
+        layout.setVerticalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE,
+                                javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE));
+
+        pack();
+    }
+
+    private void btn_calculateActionPerformed(java.awt.event.ActionEvent evt) {
+        try {
+            int numberOfNumbers = Integer.parseInt(txt_nos.getText());
+            String numbersText = txt_numbers.getText();
+            String[] numbersArray = numbersText.trim().split(" ");
+
+            if (numbersArray.length != numberOfNumbers) {
+                throw new Exception("Number of numbers and numbers entered do not match!");
+            }
+
+            int divisor = Integer.parseInt(txt_divisor.getText());
+            if (divisor == 0) {
+                throw new ArithmeticException("Cannot divide by zero!");
+            }
+
+            int sum = 0;
+            for (String number : numbersArray) {
+                sum += Integer.parseInt(number);
+            }
+
+            double result = (double) sum / divisor;
+            txt_result.setText(String.valueOf(result));
+        } catch (NumberFormatException ex) {
+            JOptionPane.showMessageDialog(this, "Invalid input! Please enter valid numbers and divisor.", "Error",
+                    JOptionPane.ERROR_MESSAGE);
+        } catch (ArithmeticException ex) {
+            JOptionPane.showMessageDialog(this, "Cannot divide by zero!", "Error", JOptionPane.ERROR_MESSAGE);
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(this,
+                    ex.getMessage() != null ? ex.getMessage() : "Something went wrong!", "Error",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    private void txt_divisorActionPerformed(java.awt.event.ActionEvent evt) {
+        return;
+    }
+
+    private void txt_resultActionPerformed(java.awt.event.ActionEvent evt) {
+        return;
+    }
+
+    public static void main(String args[]) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(ExceptionHandler.class.getName()).log(java.util.logging.Level.SEVERE,
+                    null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(ExceptionHandler.class.getName()).log(java.util.logging.Level.SEVERE,
+                    null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(ExceptionHandler.class.getName()).log(java.util.logging.Level.SEVERE,
+                    null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(ExceptionHandler.class.getName()).log(java.util.logging.Level.SEVERE,
+                    null, ex);
+        }
+
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new ExceptionHandler().setVisible(true);
+            }
+        });
+    }
+
+}
