6101f7da8d052205aac0956ba7efcc78a7ad4c6a
nandhu-44 <nandhu90040@gmail.com>
2023-08-01 19:04:00 +0530

    Completed lab cycle 2

--- Cycle-2/Question-15/SimpleCalculator.java
+++ Cycle-2/Question-15/SimpleCalculator.java
@@ -7,13 +7,13 @@ public class SimpleCalculator extends JFrame implements ActionListener {
     private JTextField num1Field, num2Field, resultField;
     private JButton calculateButton;
 
-    private JRadioButton addButton, subButton, mulButton, divButton;
+    private JRadioButton addButton, subButton, mulButton, divButton, modButton, floorDivButton;
     private ButtonGroup buttonGroup;
 
     public SimpleCalculator() {
         setTitle("Simple Calculator");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(525, 500);
+        setSize(275, 350);
         setResizable(false);
         setLayout(null);
 
@@ -25,38 +25,44 @@ public class SimpleCalculator extends JFrame implements ActionListener {
         num2Field = new JTextField();
         resultField = new JTextField();
 
-        num1Label.setBounds(100, 75, 100, 25);
-        num1Field.setBounds(100, 100, 100, 25);
+        num1Label.setBounds(25, 25, 75, 25);
+        num1Field.setBounds(25, 50, 75, 25);
         num1Field.setBorder(BorderFactory.createEmptyBorder());
 
-        num2Label.setBounds(300, 75, 100, 25);
-        num2Field.setBounds(300, 100, 100, 25);
+        num2Label.setBounds(150, 25, 75, 25);
+        num2Field.setBounds(150, 50, 75, 25);
         num2Field.setBorder(BorderFactory.createEmptyBorder());
 
-        resultLabel.setBounds(150, 275, 100, 25);
-        resultField.setBounds(150, 300, 200, 25);
+        resultLabel.setBounds(50, 185, 75, 25);
+        resultField.setBounds(50, 210, 150, 25);
         resultField.setFocusable(false);
         resultField.setBorder(BorderFactory.createEmptyBorder());
 
         calculateButton = new JButton("Calculate");
 
-        calculateButton.setBounds(200, 350, 100, 40);
+        calculateButton.setBounds(75, 250, 100, 40);
 
         addButton = new JRadioButton("+");
         subButton = new JRadioButton("-");
         mulButton = new JRadioButton("*");
         divButton = new JRadioButton("/");
+        modButton = new JRadioButton("%");
+        floorDivButton = new JRadioButton("//");
 
         buttonGroup = new ButtonGroup();
         buttonGroup.add(addButton);
         buttonGroup.add(subButton);
         buttonGroup.add(mulButton);
         buttonGroup.add(divButton);
+        buttonGroup.add(modButton);
+        buttonGroup.add(floorDivButton);
 
-        addButton.setBounds(170, 200, 50, 50);
-        subButton.setBounds(220, 200, 50, 50);
-        mulButton.setBounds(270, 200, 50, 50);
-        divButton.setBounds(320, 200, 50, 50);
+        addButton.setBounds(65, 85, 50, 50);
+        subButton.setBounds(115, 85, 50, 50);
+        modButton.setBounds(165, 85, 50, 50);
+        mulButton.setBounds(65, 135, 50, 50);
+        divButton.setBounds(115, 135, 50, 50);
+        floorDivButton.setBounds(165, 135, 50, 50);
 
         calculateButton.addActionListener(this);
 
@@ -67,10 +73,13 @@ public class SimpleCalculator extends JFrame implements ActionListener {
         this.add(num2Field);
         this.add(resultField);
         this.add(calculateButton);
+        
         this.add(addButton);
         this.add(subButton);
         this.add(mulButton);
         this.add(divButton);
+        this.add(modButton);
+        this.add(floorDivButton);
 
         this.setVisible(true);
     }
@@ -81,13 +90,18 @@ public class SimpleCalculator extends JFrame implements ActionListener {
             JOptionPane.showMessageDialog(this, "Enter both numbers", "Error", JOptionPane.ERROR_MESSAGE);
             return;
         }
-        if (!addButton.isSelected() && !subButton.isSelected() && !mulButton.isSelected() && !divButton.isSelected()) {
+        if (!addButton.isSelected() && !subButton.isSelected() && !mulButton.isSelected() && !divButton.isSelected() && !modButton.isSelected() && !floorDivButton.isSelected()) {
             JOptionPane.showMessageDialog(this, "Select an operation", "Error", JOptionPane.ERROR_MESSAGE);
             return;
         }
-        double num1 = Double.parseDouble(num1Field.getText());
-        double num2 = Double.parseDouble(num2Field.getText());
-        double result = 0;
+        double num1 = 0, num2 = 0, result = 0;
+        try {
+            num1 = Double.parseDouble(num1Field.getText());
+            num2 = Double.parseDouble(num2Field.getText());
+        } catch (NumberFormatException ex) {
+            JOptionPane.showMessageDialog(this, "Enter valid numbers", "Error", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
         if (addButton.isSelected()) {
             result = num1 + num2;
         } else if (subButton.isSelected()) {
@@ -100,6 +114,19 @@ public class SimpleCalculator extends JFrame implements ActionListener {
                 return;
             }
             result = num1 / num2;
+        } else if (modButton.isSelected()) {
+            if (num2 == 0) {
+                JOptionPane.showMessageDialog(this, "Cannot divide by zero", "Error", JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+            result = num1 % num2;
+        } else if (floorDivButton.isSelected()) {
+            if (num2 == 0) {
+                JOptionPane.showMessageDialog(this, "Cannot divide by zero", "Error", JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+            result = num1 / num2;
+            result = Math.floor(result);
         }
         int resultAsInt = (int) result;
         if (result == resultAsInt) {
